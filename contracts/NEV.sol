1d35a4efc542b4c84041c28cdbd904a23f5719ba
emregani <84226109+emregani@users.noreply.github.com>
2021-05-15 12:25:39 +0300

    Update and rename erc20_tutorial.sol to NEV.sol

--- contracts/NEV.sol
+++ contracts/NEV.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.24;
 
 // ----------------------------------------------------------------------------
-// '0Fucks' token contract
+// 'NEV' token contract
 //
-// Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222
-// Symbol      : 0FUCKS
-// Name        : 0 Fucks Token
+// Deployed to : 0x3D97960a9B26927efc97665d9B821a6347AEcE46
+// Symbol      : NEV
+// Name        : NEV
 // Total supply: 100000000
-// Decimals    : 18
+// Decimals    : 5
 //
 // Enjoy.
 //
@@ -99,7 +99,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and assisted
 // token transfers
 // ----------------------------------------------------------------------------
-contract FucksToken is ERC20Interface, Owned, SafeMath {
+contract NEV is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
@@ -113,12 +113,12 @@ contract FucksToken is ERC20Interface, Owned, SafeMath {
     // Constructor
     // ------------------------------------------------------------------------
     constructor() public {
-        symbol = "0FUCKS";
-        name = "0 Fucks Token";
-        decimals = 18;
-        _totalSupply = 100000000000000000000000000;
-        balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply;
-        emit Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply);
+        symbol = "NEV";
+        name = "NEV";
+        decimals = 8;
+        _totalSupply = 10000000000000000;
+        balances[0x3D97960a9B26927efc97665d9B821a6347AEcE46] = _totalSupply;
+        emit Transfer(address(0), 0x3D97960a9B26927efc97665d9B821a6347AEcE46, _totalSupply);
     }
 
 
